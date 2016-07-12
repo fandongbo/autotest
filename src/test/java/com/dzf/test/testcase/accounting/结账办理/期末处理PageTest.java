d74730a1518b7af0fbb960d88f90fe753e4f5065
fandongbo <dg9259@163.com>
2016-07-12 16:49:02 +0800

    期末处理-查询，成本结转

--- src/test/java/com/dzf/test/testcase/accounting/结账办理/期末处理PageTest.java
+++ src/test/java/com/dzf/test/testcase/accounting/结账办理/期末处理PageTest.java
@@ -4,10 +4,14 @@ import static org.testng.Assert.assertTrue;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
+import com.dzf.test.page.accounting.AccountingLoginPage;
 import com.dzf.test.page.accounting.AccountingMainPage;
 import com.dzf.test.page.accounting.结账办理.期末处理Page;
 import com.dzf.test.util.ILogUtil;
@@ -25,13 +29,38 @@ public class 期末处理PageTest implements ILogUtil{
 		期末处理Page = new 期末处理Page();
 	}
 	
-	@Parameters({"损益结转开始年月","损益结转结束年月"})
-	@Test(groups={"logged-in"},enabled=true)
-	public void testQiJianSunYiJieZhuan(String beginDate,String endDate) throws InterruptedException, MyException{
-		
+	@BeforeTest
+	@Parameters({ "用户名", "密码", "公司名称" })
+	public void login(String username, String password, String company) throws Exception {
+		Assert.assertTrue(new AccountingLoginPage().login(username, password, company));
+	}
+
+	@AfterTest(enabled = false)
+	public void logout() throws InterruptedException, MyException {
+		mainPage.logout();
+	}
+	
+	@Parameters({"公司","开始时间","结束时间","仅显示未结转","仅显示已结转"})
+	@Test(groups={"logged-in"},enabled=true,description = "期末处理查询", priority = 1)
+	public void search(String company, String beginDate, String endDate, boolean 仅显示未结转, boolean 仅显示已结转) throws InterruptedException, MyException{
 		mainPage.open期末处理();
+		期末处理Page.search(company, beginDate, endDate, 仅显示未结转, 仅显示已结转);
+		assertTrue(true);
+	}
+	
+	@Parameters({})
+	@Test(groups={"logged-in"},enabled=true,description = "成本结转", priority = 2)
+	public void testChengBenJieZhuan() throws InterruptedException, MyException{
+		期末处理Page.成本结转();
+		assertTrue(true);
+	}
+	
+	@Parameters({"损益结转开始年月","损益结转结束年月"})
+	@Test(groups={"logged-in"},enabled=true,description = "期间损益结转", priority = 1)
+	public void testQiJianSunYiJieZhuan(String beginDate,String endDate) throws InterruptedException, MyException{
+		//mainPage.open期末处理();
 		
-//		期末处理Page.期间损益结转(beginDate,endDate);
+		//期末处理Page.期间损益结转(beginDate,endDate);
 		
 		WebElement table = 期末处理Page.getWebElement(By.xpath(".//*[@id='dataGrid']/div/div/div/div[2]/div[2]/table"));// /tbody/tr/td[6]/div/input
 		
@@ -45,7 +74,7 @@ public class 期末处理PageTest implements ILogUtil{
 			
 			logger.info(i+":"+tableUtil.getCell(i, 1).getText()+":"+value);
 			
-			if(value.equals("是") == false){
+			if("是".equals(value) == false){
 				assertTrue(false);
 			}
 		}
