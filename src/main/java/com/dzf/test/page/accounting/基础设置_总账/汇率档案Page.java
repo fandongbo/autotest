7af3ddda2132d815ddbaef721057e69f24971d6d
fandongbo <dg9259@163.com>
2016-07-22 10:04:42 +0800

    汇率档案

--- src/main/java/com/dzf/test/page/accounting/基础设置_总账/汇率档案Page.java
+++ src/main/java/com/dzf/test/page/accounting/基础设置_总账/汇率档案Page.java
@@ -23,31 +23,19 @@ public class 汇率档案Page extends Handler {
 			throws InterruptedException, MyException {
 		try {
 			boolean result = false;
-
 			switchToDefaultContent();
-
 			switchToFrame(getWebElement("汇率档案iframe"));
 
-//			Reporter.log("检查汇率是否已设置");
 			WebTableUtil 汇率档案表格 = new WebTableUtil(getWebElement("汇率档案表格"));
-
 			for (int i = 0; i < 汇率档案表格.getRowCount(); i++) {
 				String 币种 = 汇率档案表格.getCell(i, 4).findElement(By.tagName("div")).getText();
-
 				if (币种.equals(currency)) {
-					// result.setResult(modify(currency, rate, translationMode,
-					// isFloateRate));
-					// result.setMessage(currency + "已存在");
-//					Reporter.log(currency + "已存在！增加失败！");
+					Reporter.log(currency + "已存在！增加失败！");
 					return result;
 				}
 			}
 
-//			Reporter.log("点击增加按钮");
 			click("增加按钮");
-
-			// 点击币种选择按钮
-//			Reporter.log("点击币种选择按钮");
 			click("币种选择按钮");
 
 			Thread.sleep(500);
@@ -61,78 +49,51 @@ public class 汇率档案Page extends Handler {
 
 				if (币种.equals(currency)) {
 					found = true;
-//					Reporter.log("点击" + currency + "所在的行");
+					Reporter.log("点击" + currency + "所在的行");
 					选择币种表格.getCell(j, 2).click();
 				}
 			}
 
 			if (found == false) {
-
-//				Reporter.log("没有找到该币种：" + currency + "！修改失败！");
-				// result.setMessage("没有找到该币种：" + currency);
-
-//				Reporter.log("点击取消按钮");
+				Reporter.log("没有找到该币种：" + currency + "！修改失败！");
 				click("选择币种面板-取消按钮");
-//				Reporter.log("点击取消按钮");
 				click("增加汇率信息面板-取消按钮");
 				return result;
 			}
 
-			// 点击保存按钮
-//			Reporter.log("点击保存按钮");
 			click("选择币种面板-保存按钮");
 
-			// 输入汇率
-//			Reporter.log("汇率输入框输入：" + rate);
 			input("汇率输入框", rate);
 
-			// 点击汇率折算模式选择按钮
-//			Reporter.log("点击汇率这算模式选择按钮");
 			click("汇率折算模式选择按钮");
 
-			// 点击折算模式
 			switch (translationMode) {
 			case "乘":
-			case "原币×汇率=本位币":
-//				Reporter.log("点击原币×汇率=本位币");
-				click("折算模式一");
+				click("折算模式-乘");
 				break;
 			case "除":
-			case "原币÷汇率=本位币":
-//				Reporter.log("点击原币÷汇率=本位币");
-				click("折算模式二");
+				click("折算模式-除");
 				break;
 			default:
-//				Reporter.log("没有设置折算模式，默认点击原币×汇率=本位币");
-				click("折算模式一");
+				click("折算模式-乘");
 				break;
 			}
 
-			// 点击是否浮动汇率选择按钮
-//			Reporter.log("点击是否浮动汇率选择按钮");
 			click("是否浮动汇率选择按钮");
 
-			// 点击是/否
 			switch (isFloateRate) {
 			case "是":
-//				Reporter.log("点击是");
-				click("浮动汇率选项一");
+				click("浮动汇率选项-是");
 				break;
 			case "否":
-//				Reporter.log("点击否");
-				click("浮动汇率选项二");
+				click("浮动汇率选项-否");
 				break;
 			default:
-//				Reporter.log("没有设置是否浮动，默认点击否");
-				click("浮动汇率选项二");
+				click("浮动汇率选项-否");
 				break;
 			}
 
-			// 输入备注
-//			Reporter.log("备注输入框输入" + comment);
 			input("备注输入框", comment);
-			// 点击保存按钮
-//			Reporter.log("点击保存按钮");
 			click("增加汇率信息面板-保存按钮");
 
 			Thread.sleep(1500);
@@ -149,7 +110,6 @@ public class 汇率档案Page extends Handler {
 
 			return result;
 		} catch (MyException e) {
-//			Reporter.log(e.getMessage());
 			Reporter.log("汇率添加失败！");
 			throw e;
 		}
@@ -159,123 +119,74 @@ public class 汇率档案Page extends Handler {
 			throws InterruptedException, MyException {
 		try {
 			boolean result = false;
-
 			switchToDefaultContent();
-
 			switchToFrame(getWebElement("汇率档案iframe"));
 
-//			Reporter.log("检查汇率是否已设置");
 			WebTableUtil 汇率档案表格 = new WebTableUtil(getWebElement("汇率档案表格"));
-
 			boolean currencyExist = false;
-
 			for (int i = 0; i < 汇率档案表格.getRowCount(); i++) {
 				String 币种 = getText(getWebElement(汇率档案表格.getCell(i, 4), By.tagName("div")));
 
 				if (币种.equals(currency)) {
-
-//					Reporter.log(currency + "已存在");
 					currencyExist = true;
 					click(getWebElement(汇率档案表格.getCell(i, 4), By.tagName("div")));
 				}
 			}
 
 			if (!currencyExist) {
-//				Reporter.log(currency + "不存在！修改失败！");
+				Reporter.log(currency + "不存在！修改失败！");
 				return result;
 			}
-//			Reporter.log("点击修改按钮");
 			click("修改按钮");
-
-			// 点击币种选择按钮
-//			Reporter.log("点击币种选择按钮");
 			click("币种选择按钮");
-
 			Thread.sleep(500);
-			// 点击币种名称
+			
 			WebTableUtil 选择币种表格 = new WebTableUtil(getWebElement("选择币种表格"));
-
 			boolean found = false;
-
 			for (int j = 0; j < 选择币种表格.getRowCount(); j++) {
 				String 币种 = 选择币种表格.getCell(j, 2).findElement(By.tagName("div")).getText();
 
 				if (币种.equals(currency)) {
 					found = true;
-//					Reporter.log("点击" + currency + "所在的行");
 					click(选择币种表格.getCell(j, 2));
 				}
 			}
 
 			if (found == false) {
-
-//				Reporter.log("没有找到该币种：" + currency);
-				// result.setMessage("没有找到该币种：" + currency);
-
-//				Reporter.log("点击取消按钮");
+				Reporter.log("没有找到该币种：" + currency);
 				click("选择币种面板-取消按钮");
-
-//				Reporter.log("点击取消按钮");
 				click("增加汇率信息面板-取消按钮");
-
 				return result;
 			}
-
-			// 点击保存按钮
-//			Reporter.log("点击保存按钮");
 			click("选择币种面板-保存按钮");
-
-			// 输入汇率
-//			Reporter.log("汇率输入框输入：" + rate);
 			input("汇率输入框", rate);
-
-			// 点击汇率折算模式选择按钮
-
 			click("汇率折算模式选择按钮");
-
-			// 点击折算模式
 			switch (translationMode) {
 			case "乘":
-			case "原币×汇率=本位币":
-//				Reporter.log("点击原币×汇率=本位币");
-				click("折算模式一");
+				click("折算模式-乘");
 				break;
 			case "除":
-			case "原币÷汇率=本位币":
-//				Reporter.log("点击原币÷汇率=本位币");
-				click("折算模式二");
+				click("折算模式-除");
 				break;
 			default:
-//				Reporter.log("没有设置折算模式，默认点击原币×汇率=本位币");
-				click("折算模式一");
+				click("折算模式-乘");
 				break;
 			}
-
-			// 点击是否浮动汇率选择按钮
-//			Reporter.log("点击是否浮动汇率选择按钮");
+			
 			click("是否浮动汇率选择按钮");
-
-			// 点击是/否
 			switch (isFloateRate) {
 			case "是":
-//				Reporter.log("点击是");
-				click("浮动汇率选项一");
+				click("浮动汇率选项-是");
 				break;
 			case "否":
-//				Reporter.log("点击否");
-				click("浮动汇率选项二");
+				click("浮动汇率选项-否");
 				break;
 			default:
-//				Reporter.log("没有设置是否浮动，默认点击否");
-				click("浮动汇率选项二");
+				click("浮动汇率选项-否");
 				break;
 			}
 
-			// 输入备注
-//			Reporter.log("备注输入框输入" + comment);
 			input("备注输入框", comment);
-			// 点击保存按钮
-//			Reporter.log("点击保存按钮");
 			click("增加汇率信息面板-保存按钮");
 
 			Thread.sleep(1500);
@@ -292,10 +203,39 @@ public class 汇率档案Page extends Handler {
 
 			return result;
 		} catch (MyException e) {
-			Reporter.log(e.getMessage());
-			Reporter.log("汇率添加失败！");
+			Reporter.log("汇率修改失败！");
 			throw e;
 		}
 	}
 
+	public boolean delete(String currency) throws InterruptedException, MyException{
+		try {
+			switchToDefaultContent();
+			switchToFrame(getWebElement("汇率档案iframe"));
+			WebTableUtil 汇率档案表格 = new WebTableUtil(getWebElement("汇率档案表格"));
+			boolean currencyExist = false;
+			for (int i = 0; i < 汇率档案表格.getRowCount(); i++) {
+				String 币种 = getText(getWebElement(汇率档案表格.getCell(i, 4), By.tagName("div")));
+				if (币种.equals(currency)) {
+					currencyExist = true;
+					click(getWebElement(汇率档案表格.getCell(i, 4), By.tagName("div")));
+				}
+			}
+			if (!currencyExist) {
+				Reporter.log(currency + "不存在！删除失败！");
+				return false;
+			}
+			click("删除按钮");
+			if(isDisplayed("删除提示对话框")){
+				click("删除提示对话框-确定");
+				return true;
+			}else{
+				Reporter.log(currency + "删除失败。被数据引用或系统预制不可删除。");
+				return false;
+			}
+		} catch (MyException e) {
+			Reporter.log("删除失败");
+			throw e;
+		}
+	}
 }
